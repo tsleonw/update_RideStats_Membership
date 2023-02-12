a2b1f6f0228912022c3fab125eb46932500ca47a
Leon Webster <leon@leonwebster.com>
2023-02-11 21:12:24 -0600

    modified location of deploy folder.  added deploy folder to .gitignore

--- src/ursm/deploy.py
+++ src/ursm/deploy.py
@@ -25,7 +25,7 @@ import sys
 from shutil import copy
 from zipfile import ZipFile
 
-prodDir = "/Users/tslcw/UpdateRideStats"
+prodDir = "/Users/leon/Dropbox/Projects/UpdateRideStatsMembership/deploy"
 testDir = "/Users/tslcw/Dropbox/Projects/UpdateRideStatsMembership/deployTest"
 manifest = ["src/ursm/__init__.py",
             "src/ursm/deploy.py",
